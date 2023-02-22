a249e70b9652cec76d48af3c4295f18eb897e074
Yazeed AlKhalaf <yazeedfady@gmail.com>
2023-02-22 10:35:42 +0300

    💥 big bang

---
+++ src/lib/components/ThemeButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const toggleTheme = () => {
+    document.body.classList.toggle("dark-mode");
+  };
+</script>
+
+<button on:click={toggleTheme}>Toggle Theme</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import ThemeButton from "$lib/components/ThemeButton.svelte";
+</script>
+
+<svelte:head>
+  <title>YU Tools</title>
+  <meta
+    name="description"
+    content="Tools for the students of Al Yamamah Unversity!"
+  />
+</svelte:head>
+
+<div>
+  <ThemeButton />
+</div>
